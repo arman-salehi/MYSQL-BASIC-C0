629805f0ab7b6c311fea15560eb9013b901185e4
Arman Salehi <arman.salehi@student.rocva.nl>
2020-11-19 12:30:13 +0100

    antwoorden bijgewerkt

--- 03-Delete/taak02-Primary-Keys/antwoorden.sql
+++ 03-Delete/taak02-Primary-Keys/antwoorden.sql
@@ -1,5 +1,14 @@
 -- Opdracht 3
+127.0.0.1/mod-mysql-basic-fifa2018/players/		http://localhost/phpmyadmin/tbl_sql.php?db=mod-mysql-basic-fifa2018&table=players
+ Weergave van records 0 -  1 (2 totaal, Query duurde 0,0011 seconden.)
+
+SELECT * FROM `players` WHERE id = 198329
+
+
+id	name	age	nationality	club	value	wage	
+198329	Rodrigo 	26	Spain 	Valencia CF 	13000000	36000	
+198329	Rodrigo 	26	Spain 	Valencia CF 	13000000	36000	
 
 -- Opdracht 4
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 5
